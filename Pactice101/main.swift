51f76802cbc218777ce368e41730232a9387356a
John Doe <leandro.wauters@gmail.com>
2018-10-06 12:45:54 -0400

    New

---
+++ Pactice101/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Pactice101
+//
+//  Created by Leandro Wauters on 10/6/18.
+//  Copyright © 2018 Leandro Wauters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
